5e71ab119f8f08721703dcf2b1b2ad24455b2ef1
theReneKz <rene.kultz@yahoo.com.br>
2025-11-14 17:50:27 +0000

    Exercicio Retangulo completo

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,42 @@
+public class Retangulo extends FormaGeometrica {
+
+    public Retangulo() {
+
+    }
+
+    private double largura;
+    private double altura;
+
+    public void setLargura(double largura) {
+        if(largura >= 0) {
+            this.largura = largura;
+        } else {
+            throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+    }
+
+    public double getLargura() {
+        return this.largura;
+    }
+
+    public void setAltura(double altura) {
+        if(altura >= 0) {
+            this.altura = altura;
+        } else {
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public double area(){
+        return getAltura() * getLargura();
+    }
+
+    public String toString() {
+       return String.format("[Retangulo] %.2f / %.2f", getLargura(), getAltura());
+    }
+
+}
