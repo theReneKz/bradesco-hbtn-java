297c06deee6a7a9a19d1157b59f9cc9d9db989f9
theReneKz <rene.kultz@yahoo.com.br>
2025-11-14 17:12:32 +0000

    Forma geometrica

---
+++ Heranca/forma/FormaGeometrica.java
@@ -0,0 +1,9 @@
+public class FormaGeometrica {
+    public FormaGeometrica() {
+
+    }
+
+    public double area() {
+        throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
