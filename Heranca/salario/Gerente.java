4db477b0588ad6e1b444c6d7ec94cbba44677b0e
theReneKz <rene.kultz@yahoo.com.br>
2025-11-14 17:04:01 +0000

    Execicio do salario

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -3,12 +3,11 @@ public class Gerente extends Empregado {
         super(salarioFixo);
     }
 
-    @override
-    public double calcularBonus(Departamento departamento) {
+   public double calcularBonus(Departamento departamento) {
         double bonus = 0;
         if(departamento.alcancouMeta()) {
             bonus = (this.getSalarioFixo() * 0.2) + (departamento.getValorAtingidoMeta() - departamento.getValorMeta())*0.01;
         }
-        return bonus;
-    }
+       return bonus;
+   }
 }
