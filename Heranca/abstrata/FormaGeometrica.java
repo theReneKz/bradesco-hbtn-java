877e7acc99bd2637f1bf83f7433350e91a958cff
theReneKz <renekultz@yahoo.com.br>
2025-11-14 18:41:28 +0000

    exercicio abstrata

--- Heranca/abstrata/FormaGeometrica.java
+++ Heranca/abstrata/FormaGeometrica.java
@@ -1,9 +1,7 @@
-public class FormaGeometrica {
+public abstract class FormaGeometrica {
     public FormaGeometrica() {
 
     }
 
-    public double area() {
-        throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
-    }
+    public abstract double area();
 }
