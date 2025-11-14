e47572619379353d66310945c8e4a327ef309727
theReneKz <renekultz@yahoo.com.br>
2025-11-14 18:39:05 +0000

    exercicio abstrata

---
+++ Heranca/abstrata/Quadrado.java
@@ -0,0 +1,21 @@
+public class Quadrado extends Retangulo{
+    private double lado;
+
+    public void setLado(double lado) {
+        if(lado >= 0) {
+            this.lado = lado;
+            super.altura = lado;
+            super.largura = lado;
+        } else {
+            throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+    }
+
+    public double getLado() {
+        return this.lado;
+    }
+
+    public String toString() {
+       return String.format("[Quadrado] %.2f", getLado());
+    }
+}
