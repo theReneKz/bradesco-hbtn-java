76ce80b295849e11eaf6e1411b9c8e03f2cf5a6f
theReneKz <renekultz@yahoo.com.br>
2025-11-19 18:44:33 +0000

    exercicio quadrado

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,8 +1,8 @@
 public class Quadrado {
     public static double area(double lado) {
         if(lado < 0) {
-            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo.");
-        };
+            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo");
+        }
         return lado * lado;
     }
 }
