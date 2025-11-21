13f2b64339a29d0a4ebcd5f36b8e35cf8dfa7061
theReneKz <renekultz@yahoo.com.br>
2025-11-21 18:53:15 +0000

    Excercicio read file

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -7,7 +7,7 @@ public class FileReadingExercise {
     public static void main(String[] args) {
         String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
 	
-	System.out.println("Conteúdo do arquvo '"+fileName+":\n");
+        System.out.println("Conteúdo do arquivo '"+fileName+"':\n");
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line;
