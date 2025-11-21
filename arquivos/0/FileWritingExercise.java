a62e396e3eac9493710051304be6b190cb7ece22
theReneKz <renekultz@yahoo.com.br>
2025-11-21 18:26:02 +0000

    exerccio Writer

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -6,31 +6,37 @@
 
  public class FileWritingExercise {
      public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        try {
-            // Solicita o nome do arquivo
-            System.out.print("Digite o nome do arquivo (com extensão .txt): ");
-            String fileName = scanner.nextLine();
+
+     Scanner scanner = new Scanner(System.in);
+
+
+         // Solicita o nome do arquivo
+         System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+         String fileName = scanner.nextLine();
+
+
+     // implemente o codigo aqui
+    try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
             
             String line = "";
-            while(true) {
+            while(!"sair".equals(line)) {
                 line = scanner.nextLine();
                 if("sair".equals(line)) {
-                    writer.close();
-                    System.out.println("O arquivo foi criado e seu conteúdo foi salvo com sucesso");
                     break;
+                    
                 } else {
                     writer.write(line);
                     writer.newLine();
                 }
                 
             }
-            
+            writer.close();
+            System.out.println("O arquivo foi criado e seu conteúdo foi salvo com sucesso");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            scanner.close();
         }
+
+         scanner.close();
      }
  }
