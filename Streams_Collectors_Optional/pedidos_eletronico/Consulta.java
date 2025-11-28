98db0e0f68b7dff9f6d94a8127a2b2c1b69958a8
theReneKz <renekultz@yahoo.com.br>
2025-11-28 21:21:33 +0000

    Exercicio eletronnicos

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido (Pedido pedido) {   
+        List<Produto> result = new ArrayList<>();
+        for(Produto produto: pedido.getProdutos()) {
+            if(produto.getCategoria() == CategoriaProduto.LIVRO) {
+                result.add(produto);
+            }
+        }
+        return result;
+    }
+
+    public static Produto obterProdutoMaiorPreco (List<Produto> produtos) {
+        return  produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst().get();
+
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo (List<Produto> produtos, double precoMinimo) {
+        return produtos.stream().filter((produto) -> produto.getPreco() > precoMinimo).collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream().filter((pedido) -> pedido.getProdutos().stream().anyMatch(
+                                            produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+}
