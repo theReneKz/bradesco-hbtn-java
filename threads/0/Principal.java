5822b88bf496a048aaafcbb6471f00f1184ab720
theReneKz <renekultz@yahoo.com.br>
2025-12-01 18:37:09 +0000

    Exercicio thread 0

---
+++ threads/0/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String args[]) {
+        Contador contador = new Contador();
+        ThreadContador contador1 = new ThreadContador(contador);
+        ThreadContador contador2 = new ThreadContador(contador);
+
+        contador1.run();
+        contador2.run();
+
+        System.out.println(String.format("Valor final do contador: %d", contador.getContagem()));
+    }
+}
