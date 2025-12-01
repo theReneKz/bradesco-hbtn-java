2c0b73db5b8fb4431909755b964ca78813fe4834
theReneKz <renekultz@yahoo.com.br>
2025-12-01 17:40:02 +0000

    Exercicio conta pessoas collection

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Collections;
+import java.util.stream.Collectors;
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> input) {
+        return input.stream().collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<String, TreeSet<Pessoa>>(Collections.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> input) {
+        return input.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> input) {
+        return input.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+}
